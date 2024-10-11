1dda42dd0c0307e30a4b1360000c19aa0244d7d5
Abdou Khoudoss Mbacke <khoudossmbacke18@gmail.com>
2024-10-11 21:23:57 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BsIconName } from 'react-icons/bs';
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
